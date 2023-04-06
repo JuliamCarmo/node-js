b1d3bef67361d00429d2c97220951d980233fd2f
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2023-04-06 10:13:40 -0300

    chamar arquivos html para o servidor

---
+++ exercicios/html/dirname.js
@@ -0,0 +1,15 @@
+//Criando um servidor que manda arquivos HTML
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+//sendFile() significa 'mandar arquivos'
+//a variável __dirname trás o caminho raiz (lá do explorador de arquivos) e concatena ao caminho do arquivo html)
+
+
+app.listen(8081, function(){
+  console.log("Servidor aberto!");
+});
