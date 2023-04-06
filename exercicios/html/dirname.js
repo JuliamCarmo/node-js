cf94c5d1f019286ab3915fed5abe38daf48398c7
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2023-04-06 10:19:58 -0300

    atualização

--- exercicios/html/dirname.js
+++ exercicios/html/dirname.js
@@ -6,6 +6,10 @@ app.get("/", function (req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/sobre", function(req, res){
+  res.sendFile(__dirname + "/sobre.html");
+});
+
 //sendFile() significa 'mandar arquivos'
 //a variável __dirname trás o caminho raiz (lá do explorador de arquivos) e concatena ao caminho do arquivo html)
 
